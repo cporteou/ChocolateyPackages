0d634891e5773823d530691290fa032db3db9007
Craig Porteous <Craig@CraigPorteous.com>
2019-03-20 10:28:47 -0700

    New package for Azure Data Studio

---
+++ AzureDataStudio/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url            = 'https://go.microsoft.com/fwlink/?linkid=2083323'
+  checksum       = '153BF594089CBC51D675702C4E4B575D8F71B75D2958FB456D6B4BC1576E18B8'
+  checksumType   = 'sha256'
+  fileType       = 'EXE'
+  silentArgs     = '/s'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
