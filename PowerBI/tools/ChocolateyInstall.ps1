093e9f5f9ba4d187bce88b4c7509b822e6c360f2
Craig Porteous <craig@craigporteous.com>
2019-11-15 21:28:00 +0000

    Fixed silent arg issue

--- PowerBI/tools/ChocolateyInstall.ps1
+++ PowerBI/tools/ChocolateyInstall.ps1
@@ -6,10 +6,10 @@ $packageArgs = @{
   url64bit       = 'https://download.microsoft.com/download/8/8/0/880BCA75-79DD-466A-927D-1ABF1F5454B0/PBIDesktopSetup_x64.exe'
   checksum       = '9A42793F1E4B6B1D642C05A41D1D03F49A79DA1A85F09E508A5367DE69222002'
   checksumType   = 'SHA256'
-  checksum64     = '23674359BC702FAB1F38C161E183922AF0D2AB972028564F647518AA72A862E8'
+  checksum64     = '5148216057861AFD4770B1009E5B1754E86903E329B8B1020B95201FC99578E0'
   checksumType64 = 'SHA256'
   fileType       = 'EXE'
-  silentArgs     = '/s'
+  silentArgs     = '-quiet -norestart ACCEPT_EULA=1'
   validExitCodes = @(0, 3010, 1641)
 }
 
