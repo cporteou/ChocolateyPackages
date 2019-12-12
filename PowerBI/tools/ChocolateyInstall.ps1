71be83c240db2d69e4edba2902b5603e0988abd4
teknowledgist <teknowledgist@users.noreply.github.com>
2019-12-11 23:36:21 -0500

    Made automatic
    This package should now be "automatic".  With the "au" chocolatey package installed, this powerbi package can be updated to the latest version of PowerBI simply by running the 'update.ps1' file and "push"ing the package to the Chocolatey repository.

--- PowerBI/tools/ChocolateyInstall.ps1
+++ PowerBI/tools/ChocolateyInstall.ps1
@@ -1,16 +1,15 @@
-﻿$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
-  packageName    = $env:ChocolateyPackageName
-  url            = 'https://download.microsoft.com/download/8/8/0/880BCA75-79DD-466A-927D-1ABF1F5454B0/PBIDesktopSetup.exe'
-  url64bit       = 'https://download.microsoft.com/download/8/8/0/880BCA75-79DD-466A-927D-1ABF1F5454B0/PBIDesktopSetup_x64.exe'
-  checksum       = '9A42793F1E4B6B1D642C05A41D1D03F49A79DA1A85F09E508A5367DE69222002'
-  checksumType   = 'SHA256'
-  checksum64     = '5148216057861AFD4770B1009E5B1754E86903E329B8B1020B95201FC99578E0'
-  checksumType64 = 'SHA256'
-  fileType       = 'EXE'
-  silentArgs     = '-quiet -norestart ACCEPT_EULA=1'
-  validExitCodes = @(0, 3010, 1641)
+   packageName    = $env:ChocolateyPackageName
+   url            = 'https://download.microsoft.com/download/8/8/0/880BCA75-79DD-466A-927D-1ABF1F5454B0/PBIDesktopSetup.exe'
+   url64bit       = 'https://download.microsoft.com/download/8/8/0/880BCA75-79DD-466A-927D-1ABF1F5454B0/PBIDesktopSetup_x64.exe'
+   checksum       = '60beeb08a95f51775528f1a87d4b8edeabc63f483a4cd98a2230565d17866d10'
+   checksumType   = 'SHA256'
+   checksum64     = 'c476bc9f3a6aa0cb748a69ccf9d63e33135e78ab0a483ce1c8eae2db90ac17c0'
+   fileType       = 'EXE'
+   silentArgs     = "-quiet -norestart ACCEPT_EULA=1 -log `"$env:TEMP\$env:ChocolateyPackageName.$env:chocolateyPackageVersion.Install.log`""
+   validExitCodes = @(0, 3010, 1641)
 }
 
 Install-ChocolateyPackage @packageArgs
