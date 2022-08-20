5ed94442e24efa90795ea832db2df1e127c5990e
wauterskris <105966335+wauterskris@users.noreply.github.com>
2022-08-20 20:51:58 +0200

    Update ChocolateyInstall.ps1
    updated checksums for current version 2.108.825.0

--- PowerBI/tools/ChocolateyInstall.ps1
+++ PowerBI/tools/ChocolateyInstall.ps1
@@ -4,9 +4,9 @@ $packageArgs = @{
    packageName    = $env:ChocolateyPackageName
    url            = 'https://download.microsoft.com/download/8/8/0/880BCA75-79DD-466A-927D-1ABF1F5454B0/PBIDesktopSetup.exe'
    url64bit       = 'https://download.microsoft.com/download/8/8/0/880BCA75-79DD-466A-927D-1ABF1F5454B0/PBIDesktopSetup_x64.exe'
-   checksum       = 'bb5b0150c6d091e84286ff4ca7133d16d656018be5ab473867e921639d9a8c1f'
+   checksum       = 'c51b9117d8734207f4e439e4aa9e3c7bedac0a160f2600ed8b36dccb237df7a8'
    checksumType   = 'SHA256'
-   checksum64     = 'e6b3c9180395a9151669dd682b8f6fc4215675f1396e94ca5a182131be763d59'
+   checksum64     = '0a64d3e1babcdbbff202140eae170ecf7858c6343caf8e9ce12ccb2963c4fa52'
    fileType       = 'EXE'
    silentArgs     = "-quiet -norestart ACCEPT_EULA=1 -log `"$env:TEMP\$env:ChocolateyPackageName.$env:chocolateyPackageVersion.Install.log`""
    validExitCodes = @(0, 3010, 1641)
